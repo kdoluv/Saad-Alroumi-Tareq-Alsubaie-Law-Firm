b48f43730e5be0763b06059866b35bbcd4f6f2e3
kdoluv <info@st-lawyers.com>
2026-01-12 13:29:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 5000);
+
+function setLang(lang) {
+  document.documentElement.lang = lang;
+  document.documentElement.dir = (lang === 'ar') ? 'rtl' : 'ltr';
+
+  document.querySelectorAll('[data-ar]').forEach(el => {
+    el.textContent = el.getAttribute('data-' + lang);
+  });
+}
 
