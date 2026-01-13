3334819a86ae1076fc88ec58da0627059687e81c
مكتب المحاميان سعد الرومي وطارق السبيعي <info@st-lawyers.com>
2026-01-13 15:11:15 +0300

    Update slider-lang.js

--- public_html/js/slider-lang.js
+++ public_html/js/slider-lang.js
@@ -3,9 +3,11 @@ function setLang(lang) {
   document.documentElement.lang = lang;
   document.documentElement.dir = (lang === 'ar') ? 'rtl' : 'ltr';
 
-  document.querySelectorAll('[data-ar]').forEach(el => {
-    el.textContent = el.getAttribute(`data-${lang}`);
+document.querySelectorAll('.lang-switch').forEach(btn => {
+  btn.addEventListener('click', () => {
+    setLang(btn.dataset.lang);
   });
+});
 }
 
 // SLIDER
